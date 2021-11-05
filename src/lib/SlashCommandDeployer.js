630c3dc6dae30ec24bdd9c5548d79b5a389dbbae
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-05 17:28:05 +0000

    fixed a error

--- src/lib/SlashCommandDeployer.js
+++ src/lib/SlashCommandDeployer.js
@@ -3,7 +3,7 @@ function Deployer() {
           if (slashCommandsStore) {
             try {
               console.log("Started refreshing application (/) commands.");
-              await slashCommandsStore.registerCommands();
+              slashCommandsStore.registerCommands();
               console.log("Successfully reloaded application (/) commands.");
             } catch (err) {
               console.log(err);

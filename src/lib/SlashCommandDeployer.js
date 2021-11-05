342d57f3fa8eb6c0e41fcec1a434b290977eb108
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-05 17:26:00 +0000

    filled out the deployer

--- src/lib/SlashCommandDeployer.js
+++ src/lib/SlashCommandDeployer.js
@@ -1,4 +1,14 @@
 function Deployer() {
-
+        // this function will tell the SlashCommandStore to update the global and guild commands
+          const slashCommandsStore = this.container.stores.get("slashCommands");
+          if (slashCommandsStore) {
+            try {
+              console.log("Started refreshing application (/) commands.");
+              await slashCommandsStore.registerCommands();
+              console.log("Successfully reloaded application (/) commands.");
+            } catch (err) {
+              console.log(err);
+            }
+          }
 }
 module.exports = Deployer
